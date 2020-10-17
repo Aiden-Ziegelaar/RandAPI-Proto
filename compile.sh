a89185532ce4785caaa60c64e94cb3b977b825b0
Aiden-Ziegelaar <email@example.com>
2020-10-17 16:07:09 +0800

    refactored package locations

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-protoc --go_out=./go --go_opt=paths=source_relative --go-grpc_out=./go --go-grpc_opt=paths=source_relative src/boolean.proto src/integer.proto src/service.proto
+cd src
+protoc --go_out=../go/src/dto --go_opt=paths=source_relative --go-grpc_out=../go/src/service --go-grpc_opt=paths=source_relative boolean.proto integer.proto service.proto
